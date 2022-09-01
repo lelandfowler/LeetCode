17bb1605fdb1ed23eab564d463e53fd2f4441f0d
FearIsTheMindKiller <leland.fowler.chem@gmail.com>
2022-09-01 12:23:33 -0700

    Palindrome Problem

--- PalindromeNumber/src/Solution.java
+++ PalindromeNumber/src/Solution.java
@@ -1,14 +1,48 @@
 //Constraints -231*10^31 <= x <= 231*10^31 - 1
-import java.util.Arrays;
+
+import java.math.BigInteger;
 
 class Solution {
     public Solution() {
     }
 
+//    public boolean isPalindrome(int x) {
+//        String asString = String.valueOf(x);
+//        BigInteger as_Big = new BigInteger(asString);
+//        BigInteger working_value = new BigInteger(asString);
+//        int length = asString.length();
+//        BigInteger reverse = new BigInteger("0");
+//        BigInteger base = new BigInteger("10");
+//        while(working_value.compareTo(BigInteger.valueOf(1))>=0){
+//            BigInteger mod = working_value.remainder(base);
+//            BigInteger power = new BigInteger(String.valueOf((int) Math.pow(10,length-1)));
+//            reverse =  reverse.add(mod.multiply(power));
+//            working_value= working_value.divide(base);
+//            length-=1;
+//        }
+//        return reverse.compareTo(as_Big)==0;
+//    }
+
     public boolean isPalindrome(int x) {
-        byte a = (byte) x;
-        String s1 = String.format("%8s", Integer.toBinaryString(a & 0xFF)).replace(' ', '0');
-        System.out.println(s1);
-        return false;
+//        case #1: 0-9
+        if(x >= 0 && x <= 9) return true;
+//        case #2: negitive or ending in zero
+        if(x < 0 || x % 10 == 0) return false;
+
+//        build half of the possible palindrome
+        int result = 0;
+        while(result < x){
+            result = result * 10 + x % 10;
+            x/=10;
+        }
+
+//        case #3: if odd length
+        if (result > x){
+            return result/10 == x;
+        }
+//        case #4: else if even length
+        else{
+            return result == x;
+        }
     }
 }
--- PalindromeNumber/src/Tester.java
+++ PalindromeNumber/src/Tester.java
@@ -7,19 +7,40 @@ class Tester {
         int input_3 = 10;
         int input_4 = 232;
         int input_5 = 79597;
-        int input_6 = 123454321;
-//        BigInteger input_7 = new BigInteger("1111111123456789876543211111111");
-//        BigInteger input_8 = new BigInteger("-1111111123456789876543211111111");
+        int input_6 = 345676543;
         boolean expected_output_1 = true;
         boolean expected_output_2 = false;
         boolean expected_output_3 = false;
+        boolean expected_output_4 = true;
+        boolean expected_output_5 = true;
+        boolean expected_output_6 = true;
+        boolean expected_output_7 = false;
 
-        int[] inputs = new int[]{input_1,input_2, input_3};
-        boolean[] expected_outputs = new boolean[]{expected_output_1, expected_output_2, expected_output_3};
+
+        int[] inputs = new int[]{
+                input_1,
+                input_2,
+                input_3,
+                input_4,
+                input_5,
+                input_6,
+        };
+
+        boolean[] expected_outputs = new boolean[]{
+                expected_output_1,
+                expected_output_2,
+                expected_output_3,
+                expected_output_4,
+                expected_output_5,
+                expected_output_6
+        };
 
         for(int i = 0; i < inputs.length; i++) {
             Solution test = new Solution();
-            test.isPalindrome(inputs[i]);
+            boolean result = test.isPalindrome(inputs[i]);
+            boolean matches_expected = result == expected_outputs[i];
+            System.out.println("Test#"+(i+1)+" matches expected result: "+matches_expected);
+
         }
     }
 }
