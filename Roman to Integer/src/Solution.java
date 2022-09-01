384343e5b3e7d5fb38aa3600a651e85df9a9197d
FearIsTheMindKiller <leland.fowler.chem@gmail.com>
2022-09-01 15:08:50 -0700

    Roman Numerals, optimized

--- Roman to Integer/src/Solution.java
+++ Roman to Integer/src/Solution.java
@@ -6,43 +6,34 @@ import java.util.*;
 
 class Solution {
     public int romanToInt(String s) {
-        if (s.length() <= 15){
-            Map<String, Integer> roman_numinary = new HashMap<>(){};
-            roman_numinary.put("I",1);
-            roman_numinary.put("V",5);
-            roman_numinary.put("X",10);
-            roman_numinary.put("L",50);
-            roman_numinary.put("C",100);
-            roman_numinary.put("D",500);
-            roman_numinary.put("M",1000);
-
-            int value_holder = 0;
-            int last_value = 0;
-            int total_value = 0;
-
-//            Start from right and build upwards
-            for(int i = s.length() - 1; i>=0; i--){
-                char current_character = s.charAt(i);
-                String current_character_as_string = String.valueOf(current_character);
-                int current_value = roman_numinary.get(current_character_as_string);
-                // If the last value is less than the current value
-                if(current_value == last_value){
-                    value_holder+= last_value;
-                    last_value = current_value;
-                } else if (current_value < last_value) {
-                    value_holder-= current_value;
-                    last_value = current_value;
-                } else if (current_value > last_value) {
-                    total_value+= current_value;
-                    total_value+=value_holder;
-                    last_value = current_value;
-                    value_holder = 0;
-
-                }
+        int numeral_as_number = 0;
+        for(int i = 0; i < s.length(); i++){
+            int value = romanNuminary(s.charAt(i));
+            int last_value = 1000;
+            if(value>last_value){
+                numeral_as_number+=value;
             }
-            total_value+=value_holder;
-            return total_value;
         }
-        else return -1;
+        return numeral_as_number;
+    }
+    private int romanNuminary ( char c){
+        switch (c) {
+            case 'I':
+                return 1;
+            case 'V':
+                return 5;
+            case 'X':
+                return 10;
+            case 'L':
+                return 50;
+            case 'C':
+                return 100;
+            case 'D':
+                return 500;
+            case 'M':
+                return 1000;
+            default:
+                return 0;
+        }
     }
 }
